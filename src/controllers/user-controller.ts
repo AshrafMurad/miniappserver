01b2b5180e9c4e73507fa05848b98b1a7c7986d3
Ashraf <ashrafmurad7@gmail.com>
2024-08-03 18:52:19 +0300

    just 22

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -27,7 +27,7 @@ export const signupUser = async(req: Request, res: Response, next) =>{
 
     res.clearCookie(COOKIE_NAME , {
       httpOnly: true,
-      domain: 'vercel.app',
+      domain: 'miniapp-sable.vercel.app',
       signed: true,
       path: '/',
     })
@@ -63,7 +63,7 @@ export const loginUser = async(req:Request, res:Response, next) =>{
     }
     res.clearCookie(COOKIE_NAME , {
       httpOnly: true,
-      domain: 'vercel.app',
+      domain: 'miniapp-sable.vercel.app',
       signed: true,
       path: '/'
     })
@@ -72,7 +72,7 @@ export const loginUser = async(req:Request, res:Response, next) =>{
     expires.setDate(expires.getDate() + 7)
     res.cookie(COOKIE_NAME, token, {
       path:'/',
-      domain: 'vercel.app',
+      domain: 'miniapp-sable.vercel.app',
       expires,
       httpOnly: true,
       signed: true,
@@ -110,7 +110,7 @@ export const userLogout = async(req:Request, res:Response, next) =>{
     } 
     res.clearCookie(COOKIE_NAME , {
       httpOnly: true,
-      domain: 'vercel.app',
+      domain: 'miniapp-sable.vercel.app',
       signed: true,
       path: '/'
     })
