7864319fc4dd65a27278ada162eeac31c0ab1fbc
Ashraf <ashrafmurad7@gmail.com>
2024-08-03 21:31:01 +0300

    just 32

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -35,7 +35,7 @@ export const signupUser = async (req: Request, res: Response, next) => {
       path: "/",
       expires,
       httpOnly: true,
-
+      signed: true
     });
 
     return res
@@ -74,6 +74,7 @@ export const loginUser = async (req: Request, res: Response, next) => {
       path: "/",
       expires,
       httpOnly: true,
+      signed: true
     });
     return res
       .status(200)
